88ea7c96183931dc9574a2c6deb0eefa618cc959
Wendrick Figueiredo <115407077+Wendrick-Figueiredo@users.noreply.github.com>
2024-10-02 12:39:33 -0300

    Git

--- 1_intro/index.ts
+++ 1_intro/index.ts
@@ -7,3 +7,14 @@ function greeting(name) {
 }
 
 greeting(firstName)
+
+function soma(a: number, b: number){
+   return a + b 
+}
+
+console.log(soma(2, 2))
+
+const z = 5
+const y = 8
+
+console.log(soma(z, y))
---
+++ 2_fundamentos/index.ts
@@ -0,0 +1,5 @@
+// 1- numbers
+let x: number = 10
+
+console.log(x)
+
